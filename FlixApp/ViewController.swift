c8fe477461805fb275c726dc63f539d3cf0e1193
johnnydg13 <88180889+johnnydg13@users.noreply.github.com>
2022-02-27 18:36:26 -0800

    updated

--- FlixApp/ViewController.swift
+++ FlixApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
    
@@ -63,9 +64,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
-        let posterUrl = URL(string: baseUrl + posterPath)
+        let posterUrl = URL(string: baseUrl + posterPath!)
         
-        
+        cell.posterView.af_setImage(withURL: posterUrl)
         
         return cell
     }
